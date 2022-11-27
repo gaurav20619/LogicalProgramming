0e036ac160cb92dca4c8eb24b329c503ba0475e0
gaurav20619 <gauravsinghraijada619@gmail.com>
2022-11-27 21:24:28 +0530

    Add: Fibonacci series program

---
+++ src/main/java/com/bridgelabz/logicalprograms/LogicalprogramsApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.logicalprograms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicalprogramsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogicalprogramsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/logicalprograms/LogicalprogramsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.logicalprograms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogicalprogramsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
